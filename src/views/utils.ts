708bc4a3f743865690780aea04df9d3b30f0760c
Albert <github@albert.sh>
2020-09-24 14:05:09 +0200

    Fix balance display bug

--- src/views/utils.ts
+++ src/views/utils.ts
@@ -2,5 +2,9 @@ export default function displayTokenValue(amount: string, tokenDecimals:number):
     const padded = amount.padStart(tokenDecimals + 1, '0');
     const decimals = padded.substr(-tokenDecimals).replace(/0+$/, '')
     const int = padded.substr(0, padded.length - tokenDecimals);
-    return `${int}.${decimals}`
+    if(decimals===''){
+        return int
+    } else {
+        return `${int}.${decimals}`
+    }  
 }
